f48434ee795c2c7874f1d4798b0a9a6b1fbc2dcd
lishuai-cau <85425167+lishuai-cau@users.noreply.github.com>
2023-03-25 18:52:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 import cv2
 import os
 import  numpy as np
-global ori_img,ir_img,color_img,point,count
+global ori_img,ir_img,color_img,point,count,filename
 import  os
 from numpy.linalg import inv
 import platform
@@ -19,7 +19,7 @@ def img_perspect_transform(img, M):
     return cv2.warpPerspective(img, M, img_size)
 
 def on_mouse(event,x,y,flags,param):
-    global color_img,ir_img,ori_img,point,count
+    global color_img,ir_img,ori_img,point,count,filename
     # img=ori_img.copy()
     if event==cv2.EVENT_LBUTTONDOWN:#左键点击
         count = count+1
@@ -29,14 +29,14 @@ def on_mouse(event,x,y,flags,param):
         else:
             point[count-1]=(x,y)
             cv2.circle(ori_img, point[count-1], 10, (0, 255, 0), 5)
-        cv2.imshow('oriimg',ori_img)
+        cv2.imshow(filename,ori_img)
 
 
     elif event==cv2.EVENT_LBUTTONUP:#左键释放
         if count %2 ==0:
             print("point1 to point2",point[count-2],point[count-1])
             cv2.line(ori_img,point[count-2],point[count-1],(0,0,255),3)
-            cv2.imshow('oriimg', ori_img)
+            cv2.imshow(filename, ori_img)
         if count % 8 == 0:
 
             four_points = [(point[count - 7][0]-384,point[count - 7][1]),(point[count - 5][0]-384,point[count - 5][1]),
@@ -50,18 +50,19 @@ def on_mouse(event,x,y,flags,param):
             H_four_points = np.subtract(np.array(pre_four_points), np.array(four_points))
             datum = (training_image, np.array(four_points), H_four_points)
             print(datum)
-            cv2.imshow("perspect",combine)
+            # cv2.imshow("perspect",combine)
             print(H)
             if not os.path.exists(new_path):
                 os.makedirs(new_path)
-            np.save(new_path + '/' + ('%s' % (count/8)).zfill(6), datum)
+            np.save(new_path + '/' + ('%s' % int(count/8)).zfill(6), datum)
 
 def read_path(file_pathname,mode = 'gray'):
     color_filepath = file_pathname + '/color'
     ir_filepath = file_pathname + '/ir'
     #遍历该目录下的所有图片文件
+    global ori_img, ir_img, color_img, filename
     for filename in os.listdir(color_filepath):
-        global ori_img, ir_img, color_img
+
         if mode == 'gray':
             color_img = cv2.imread(color_filepath+'/'+filename,0)
             ir_img = cv2.imread(ir_filepath+'/'+filename,0)
@@ -76,8 +77,10 @@ def read_path(file_pathname,mode = 'gray'):
         ori_img = np.hstack((color_img,ir_img))
         cv2.imshow(filename,ori_img)
         if platform.system().lower() == 'windows':
+            print("current platform is windows")
             cv2.waitKey(0)
         elif platform.system().lower() == 'linux':
+            print("current platform is linux")
             while cv2.waitKey(100) != 27:
                 if cv2.getWindowProperty(filename, cv2.WND_PROP_VISIBLE) <= 0:
                     break
